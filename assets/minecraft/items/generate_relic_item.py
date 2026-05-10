46ce383f5b7fb97c3a0b2449fe88326c4482c9fd
en52sal <90728997+en52sal@users.noreply.github.com>
2026-05-10 06:59:44 -0400

    New Relics + Icons

--- assets/minecraft/items/generate_relic_item.py
+++ assets/minecraft/items/generate_relic_item.py
@@ -1,21 +1,26 @@
 import os
 
-relics = [
-    "quick_reload",
-    "speed",
-    "springboard",
-    "taco_time",
-    "superball",
-    "hearty",
-    "bomb",
-    "big_border",
-    "entrapped",
-    "flaregun",
-    "snack"
-]
-
-
 template = '''{{"when": "{when}","model": {{"type": "minecraft:condition","on_false": {{"type": "minecraft:model","model": "minecraft:item/relics/{when}","tints": [{{"type": "minecraft:custom_model_data","index": 1,"default": 4294967295}}]}},"on_true": {{"type": "minecraft:model","model": "minecraft:item/relics/gray/{when}","tints": [{{"type": "minecraft:custom_model_data","index": 1,"default": 4294967295}}]}},"property": "minecraft:custom_model_data","index": 1}}}}'''
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Go to textures/item/relics
+relics_dir = os.path.abspath(
+    os.path.join(
+        script_dir,
+        "..",                 # minecraft
+        "textures",
+        "item",
+        "relics"
+    )
+)
+
+# Get all png files (no subfolders)
+relics = [
+    os.path.splitext(file)[0]
+    for file in os.listdir(relics_dir)
+    if file.endswith(".png")
+]
 
 entries = []
 
@@ -26,8 +31,9 @@ for r in relics:
 
 output = ",\n".join(entries)
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
 output_path = os.path.join(script_dir, "result.json")
 
 with open(output_path, "w", encoding="utf-8") as f:
     f.write(output)
+
+print(f"Generated {len(relics)} entries.")
